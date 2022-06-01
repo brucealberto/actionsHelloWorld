5a554123e7befbc647f4cab22b32eca3686d6858
bruce alberto <bruce@DESKTOP-CCGICPI.localdomain>
2022-06-01 17:56:32 -0300

    first commit aula25_1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+// require('dotenv')
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (_req, res) =>
+  res.status(200).json({ message: "O Bruce é muito Inteligente!" })
+);
+
+app.listen(port, () => console.log(`Running on port ${port}`));
